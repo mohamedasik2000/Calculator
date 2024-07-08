b14aea6a5b7fbf9159a12ac05f71952b0741fd6d
mohamedasik2000 <131941366+mohamedasik2000@users.noreply.github.com>
2024-07-08 23:57:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function displayValue(val) {
+    console.log(document.getElementById("display").value)
+    document.getElementById("display").value = document.getElementById("display").value + val;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = ""
+}
+
+function Calculate() {
+    var userInput = document.getElementById("display").value;
+    var result = eval(userInput);
+    document.getElementById("display").value = result;
+}
